bdafcddd79b8070be777c42fcc31fea54ab7e0d0
durgasankar <durgasankar.raja500@gmail.com>
2020-02-11 11:07:22 +0530

    [durgasankar].add -> two way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { EventBindingComponent } from './event-binding/event-binding.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { EventBindingComponent } from "./event-binding/event-binding.component";
+import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventBindingComponent
+    EventBindingComponent,
+    TwoWayDataBindingComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
