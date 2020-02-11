0e3c9ba452c8d525ebfc850e4818a94630cfc68b
durgasankar <durgasankar.raja500@gmail.com>
2020-02-11 11:57:14 +0530

    [durgasankar].add -> bootsttrap date picker added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,29 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 // for two way data binding
 import { FormsModule } from "@angular/forms";
+// bootstrap date picker
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { EventBindingComponent } from "./event-binding/event-binding.component";
 import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgxBootStrapDemoComponent } from './ngx-boot-strap-demo/ngx-boot-strap-demo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventBindingComponent,
-    TwoWayDataBindingComponent
+    TwoWayDataBindingComponent,
+    NgxBootStrapDemoComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    BsDatepickerModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/ngx-boot-strap-demo/ngx-boot-strap-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ngx-boot-strap-demo',
+  templateUrl: './ngx-boot-strap-demo.component.html',
+  styleUrls: ['./ngx-boot-strap-demo.component.css']
+})
+export class NgxBootStrapDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
