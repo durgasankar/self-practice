7b0dce0cc834eb0d595128fc714b0108878d6529
durgasankar <durgasankar.raja500@gmail.com>
2020-02-11 19:35:30 +0530

    [durgasankar].add -> routing practice added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EventBindingComponent } from "./event-binding/event-binding.component";
+import { NgIfDirectiveComponent } from "./ng-if-directive/ng-if-directive.component";
+import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: TwoWayDataBindingComponent },
+  { path: "home", component: EventBindingComponent },
+  { path: "aditi", component: NgIfDirectiveComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const RoutingComponent = [
+  TwoWayDataBindingComponent,
+  EventBindingComponent,
+  NgIfDirectiveComponent
+];
