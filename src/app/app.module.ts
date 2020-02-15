1d7d8b359a8e72e264a6a185d95498c0c4dae269
durgasankar <durgasankar.raja500@gmail.com>
2020-02-15 13:02:03 +0530

    [durgasankar].add -> data fetching from http clent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,27 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 // bootstrap date picker
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+// Http client
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { EventBindingComponent } from "./event-binding/event-binding.component";
 import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NgxBootStrapDemoComponent } from './ngx-boot-strap-demo/ngx-boot-strap-demo.component';
-import { NgIfDirectiveComponent } from './ng-if-directive/ng-if-directive.component';
-import { NgSwitchComponent } from './ng-switch/ng-switch.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
-import { LoginComponent } from './login/login.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SchoolComponent } from './about/school/school.component';
-import { CollegeComponent } from './about/college/college.component';
-import { BuyProductsComponent } from './buy-products/buy-products.component';
-import { ParentAppComponent } from './parent-app/parent-app.component';
-import { ChildAppComponent } from './parent-app/child-app/child-app.component';
+import { NgxBootStrapDemoComponent } from "./ngx-boot-strap-demo/ngx-boot-strap-demo.component";
+import { NgIfDirectiveComponent } from "./ng-if-directive/ng-if-directive.component";
+import { NgSwitchComponent } from "./ng-switch/ng-switch.component";
+import { HomeComponent } from "./home/home.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AboutComponent } from "./about/about.component";
+import { LoginComponent } from "./login/login.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { SchoolComponent } from "./about/school/school.component";
+import { CollegeComponent } from "./about/college/college.component";
+import { BuyProductsComponent } from "./buy-products/buy-products.component";
+import { ParentAppComponent } from "./parent-app/parent-app.component";
+import { ChildAppComponent } from "./parent-app/child-app/child-app.component";
+import { HttpPracticeComponent } from './http-practice/http-practice.component';
 
 @NgModule({
   declarations: [
@@ -40,11 +43,13 @@ import { ChildAppComponent } from './parent-app/child-app/child-app.component';
     CollegeComponent,
     BuyProductsComponent,
     ParentAppComponent,
-    ChildAppComponent
+    ChildAppComponent,
+    HttpPracticeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
     BsDatepickerModule.forRoot()
---
+++ src/app/http-practice/http-practice.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "app-http-practice",
+  templateUrl: "./http-practice.component.html",
+  styleUrls: ["./http-practice.component.css"]
+})
+export class HttpPracticeComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  userName: string = "";
+  response: any;
+  isDataAvailabe: boolean = false;
+
+  ngOnInit() {
+    //   this.http
+    //     .get("https:api.github.com/users/durgasankar")
+    //     .subscribe(resp => console.log(resp));
+  }
+  search() {
+    console.log("Username : " + this.userName);
+    this.http
+      .get("https:api.github.com/users/" + this.userName)
+      .subscribe(response => {
+        console.log(response);
+        this.response = response;
+        this.isDataAvailabe = true;
+      });
+  }
+}
