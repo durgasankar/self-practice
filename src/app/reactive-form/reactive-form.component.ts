346fdd1dd6be873641b68163ee63bf4519c6bb3f
durgasankar <durgasankar.raja500@gmail.com>
2020-02-20 12:09:25 +0530

    [durgasankar].add -> angular reactive form formBuilder

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,36 +1,46 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl } from "@angular/forms";
-
+import { FormBuilder } from "@angular/forms";
+// import { FormGroup, FormControl } from "@angular/forms";
 @Component({
   selector: "app-reactive-form",
   templateUrl: "./reactive-form.component.html",
   styleUrls: ["./reactive-form.component.css"]
 })
 export class ReactiveFormComponent implements OnInit {
-  constructor() {}
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit() {}
-  registrationForm = new FormGroup({
-    // we can add default value
-    userName: new FormControl(""),
-    password: new FormControl(""),
-    confirmPassword: new FormControl("")
-    // address: new FormGroup({
-    //   city: new FormControl(""),
-    //   state: new FormControl(""),
-    //   pinCode: new FormControl("")
-    // })
+  // registrationForm = new FormGroup({
+  //   // we can add default value
+  //   userName: new FormControl(""),
+  //   password: new FormControl(""),
+  //   confirmPassword: new FormControl("")
+  //   // address: new FormGroup({
+  //   //   city: new FormControl(""),
+  //   //   state: new FormControl(""),
+  //   //   pinCode: new FormControl("")
+  //   // })
+  // });
+  // loadApiData() {
+  //   this.registrationForm.setValue({
+  //     userName: "raja",
+  //     password: "raja0000",
+  //     confirmPassword: "raja0000"
+  //     // address: {
+  //     //   city: "uganda",
+  //     //   state: "ramshejade",
+  //     //   pincode: "5468425"
+  //     // }
+  //   });
+  // }
+  registrationForm = this.formBuilder.group({
+    userName: ["raja"],
+    password: ["raja0000"],
+    confirmPassword: ["raja0000"],
+    address: this.formBuilder.group({
+      city: "uganda",
+      state: "ramshejade",
+      pincode: "5468425"
+    })
   });
-  loadApiData() {
-    this.registrationForm.setValue({
-      userName: "raja",
-      password: "raja0000",
-      confirmPassword: "raja0000"
-      // address: {
-      //   city: "uganda",
-      //   state: "ramshejade",
-      //   pincode: "5468425"
-      // }
-    });
-  }
 }
