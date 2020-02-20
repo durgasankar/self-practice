ec5530361dbc02be1362b128f417566d7eeef804
durgasankar <durgasankar.raja500@gmail.com>
2020-02-20 11:58:29 +0530

    [durgasankar].add -> angular reactive form builder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 // for two way data binding
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 // bootstrap date picker
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 // Http client
+
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -26,8 +27,8 @@ import { ParentAppComponent } from "./parent-app/parent-app.component";
 import { ChildAppComponent } from "./parent-app/child-app/child-app.component";
 import { HttpPracticeComponent } from "./http-practice/http-practice.component";
 import { MaterialModule } from "./material/material.module";
-import { AngFormComponent } from './ang-form/ang-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+import { AngFormComponent } from "./ang-form/ang-form.component";
+import { ReactiveFormComponent } from "./reactive-form/reactive-form.component";
 
 @NgModule({
   declarations: [
@@ -52,6 +53,8 @@ import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
     ReactiveFormComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
     BrowserModule,
     MaterialModule,
     AppRoutingModule,
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-reactive-form',
-  templateUrl: './reactive-form.component.html',
-  styleUrls: ['./reactive-form.component.css']
+  selector: "app-reactive-form",
+  templateUrl: "./reactive-form.component.html",
+  styleUrls: ["./reactive-form.component.css"]
 })
 export class ReactiveFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  registrationForm = new FormGroup({
+    // we can add default value
+    userName: new FormControl(""),
+    password: new FormControl(""),
+    confirmPassword: new FormControl("")
+    // address: new FormGroup({
+    //   city: new FormControl(""),
+    //   state: new FormControl(""),
+    //   pinCode: new FormControl("")
+    // })
+  });
+  loadApiData() {
+    this.registrationForm.setValue({
+      userName: "raja",
+      password: "raja0000",
+      confirmPassword: "raja0000"
+      // address: {
+      //   city: "uganda",
+      //   state: "ramshejade",
+      //   pincode: "5468425"
+      // }
+    });
   }
-
 }
