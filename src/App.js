2bd644653f745e89d15edfc9937896fdd13d602e
durgasankar_08 <Durgasankar.Mishra@godigit.com>
2020-07-22 13:40:58 +0530

    [durgasankar].add -> exporting file with default and arrow function and importing in other js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Welcome } from './components/demo';
+import Greet from './components/demo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Welcome />
+     2nd component
+     <Greet /> 
     </div>
   );
 }
